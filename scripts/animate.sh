2bbe23751616ee818733eda955e35c9c40fd3e04
aparna Agrawal <aparna.agrawal@research.iiit.ac.in>
2024-03-01 11:17:19 +0530

    some changes

--- scripts/animate.sh
+++ scripts/animate.sh
@@ -1,1 +1,2 @@
-python3 -m magicanimate.pipelines.animation --config configs/prompts/animation.yaml
+export CUDA_VISIBLE_DEVICES=1 
+python3 -m magicanimate.pipelines.animation_original --config configs/prompts/animation.yaml
